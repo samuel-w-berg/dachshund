b42a47fb80ac804746044bbd300236bd27a98005
samuel-w-berg <samuel.w.berg@gmail.com>
2023-04-10 10:45:36 -0500

    Add model and admin page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Rock(models.Model):
+    name= models.CharField(max_length=100)
+    type= models.CharField(max_length=100)
+    description= models.CharField(max_length=250)
--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from .models import Rock
 
 
-#Temporary Model
-class Rock:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, type, description):
-    self.name = name
-    self.type = type
-    self.description = description
-
-rocks = [
-  Rock('Granite', 'igneous', 'cooled magma with large rock crystals'),
-  Rock('Sandstone', 'sedimentary', 'sand is accumulated and packed together over time'),
-  Rock('Schist', 'metamorphic', 'foliated layers that are easily flaked off')
-]
 
 def home(request):
   return HttpResponse('<h1>Hello World!</h1>')
 
 def rocks_index(request):
+    rocks = Rock.objects.all()
     return render(request, 'rocks/index.html', {'rocks':rocks})
