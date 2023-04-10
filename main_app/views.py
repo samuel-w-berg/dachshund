404faf4d1ded8340048d7d7ee36a213eb637b31c
samuel-w-berg <samuel.w.berg@gmail.com>
2023-04-10 07:12:33 -0500

    Project is now about rocks instead of dachshunds

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+#Temporary Model
+class Rock:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, type, description):
+    self.name = name
+    self.type = type
+    self.description = description
+
+rocks = [
+  Rock('Granite', 'igneous', 'cooled magma with large rock crystals'),
+  Rock('Sandstone', 'sedimentary', 'sand is accumulated and packed together over time'),
+  Rock('Schist', 'metamorphic', 'foliated layers that are easily flaked off')
+]
+
+def home(request):
+  return HttpResponse('<h1>Hello World!</h1>')
--- rockcollector/wsgi.py
+++ rockcollector/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dachshund.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rockcollector.settings')
 
 application = get_wsgi_application()
