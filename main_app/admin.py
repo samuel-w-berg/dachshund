1a44edeb9e95f79b26a3c470c88c8b9dbadf04c6
samuel-w-berg <samuel.w.berg@gmail.com>
2023-04-11 13:01:20 -0500

    Observation model and view

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Rock
+from .models import Rock, Observation
 
 admin.site.register(Rock)
+admin.site.register(Observation)
--- main_app/models.py
+++ main_app/models.py
@@ -9,3 +9,15 @@ class Rock(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'rock_id':self.id})
+    
+    def __str__(self):
+        return f"Name: {self.name}, Type: {self.type}"
+    
+
+class Observation(models.Model):
+    date= models.DateField()
+    observation= models.CharField(max_length=250)
+    rock = models.ForeignKey(Rock, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Date: {self.date}, Observation: {self.observation}"
